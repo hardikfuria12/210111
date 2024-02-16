3104cf9cf0fc4b84e25e5da934c1b4488b399149
Hardik Furia <hardikfuria12@gmail.com>
2024-02-16 14:51:51 +0530

    this version has separate kv file and input

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.utils import platform
 from kivy.logger import Logger
-
+from kivymd.uix.screenmanager import MDScreenManager, ScreenManager
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.uix.button import MDFlatButton
 # Logger.info(f"PLATFORM {kivy.utils.platform}")
 if platform == 'android':
     Logger.info("  ON ANDROID - REQUESTING PERMISSIONS")
@@ -15,17 +18,25 @@ if platform == 'android':
 
 # KV string that describes the layout
 KV = '''
-Screen:
-    MDLabel:
-        text: "Hello, World!"
-        halign: 'center'
-        valign: 'center'
+MDScreenManager:
+    MDScreen:
+        spacing: "15px"
+        MDBoxLayout:
+            adaptive_height: True
+            orientation: "vertical"
+            MDTextField:
+                hint_text: "username"
+            MDTextField:
+                hint_text: "password"
+            MDFlatButton:
+                text: "login"
+        Widget:
 '''
-
+MAIN_KV = "./templates/main.kv"
 class HelloWorldApp(MDApp):
     def build(self):
         # Load the KV string
-        return Builder.load_string(KV)
+        return Builder.load_file(MAIN_KV)
 
 if __name__ == '__main__':
     HelloWorldApp().run()
