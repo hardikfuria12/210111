a9161b2b6ea2d0e1be399b7b66d062b635dde325
Hardik Furia <hardikfuria12@gmail.com>
2024-02-18 11:05:09 +0530

    camera stuff, works on macOs not on android device

--- buildozer.spec
+++ buildozer.spec
@@ -96,7 +96,7 @@ fullscreen = 1
 # (list) Permissions
 # (See https://python-for-android.readthedocs.io/en/latest/buildoptions/#build-options-1 for all the supported syntaxes and properties)
 #android.permissions = android.permission.INTERNET, (name=android.permission.WRITE_EXTERNAL_STORAGE;maxSdkVersion=18)
-android.permissions = android.permission.INTERNET, android.permission.ACCESS_NETWORK_STATE
+android.permissions = android.permission.INTERNET, android.permission.ACCESS_NETWORK_STATE, android.permission.CAMERA,
 
 # (list) features (adds uses-feature -tags to manifest)
 #android.features = android.hardware.usb.host
--- main.py
+++ main.py
@@ -6,37 +6,42 @@ from kivymd.uix.screenmanager import MDScreenManager, ScreenManager
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.button import MDFlatButton
+
+from kivymd.uix.screen import MDScreen
 # Logger.info(f"PLATFORM {kivy.utils.platform}")
 if platform == 'android':
-    Logger.info("  ON ANDROID - REQUESTING PERMISSIONS")
+    Logger.info("ON ANDROID - REQUESTING PERMISSIONS")
     from android.permissions import Permission, request_permissions
     Logger.info("    imported permission things...")
-    perms = [Permission.INTERNET]
+    perms = [Permission.INTERNET, Permission.CAMERA]
     Logger.info(f"   asking for `{perms}`...")
     res = request_permissions(perms, None)
     Logger.info(f"    permissions requested {res} ...")
 
-# KV string that describes the layout
-KV = '''
-MDScreenManager:
-    MDScreen:
-        spacing: "15px"
-        MDBoxLayout:
-            adaptive_height: True
-            orientation: "vertical"
-            MDTextField:
-                hint_text: "username"
-            MDTextField:
-                hint_text: "password"
-            MDFlatButton:
-                text: "login"
-        Widget:
-'''
 MAIN_KV = "./templates/main.kv"
+
+from screens.attendance_screen import AttendanceScreen
 class HelloWorldApp(MDApp):
+    # One user is the actor of an app
+    # user_id attribute
+
+    # ideal time cutoff period automatic logout
+
+    def validate_login(self, username, password, instance):
+        if username == "rlad" and password == "admin":
+            # JOURNEY BEGINS -- CUE IN ANIMATIONS
+            # assign user_id based on login information as an app atribute
+
+            sm = instance.manager
+            sm.current = "attendance_screen"
+
+    def user_attendance(self):
+        # navigate to attendance journey screen
+        pass 
+
     def build(self):
         # Load the KV string
         return Builder.load_file(MAIN_KV)
-
+  
 if __name__ == '__main__':
     HelloWorldApp().run()
